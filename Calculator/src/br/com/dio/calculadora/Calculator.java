6ac35ce9d5e14004fee4f3ed26655ebcf35f8092
Rodrigo Santos <63327335+rodrigobrito93@users.noreply.github.com>
2021-08-29 17:18:00 -0300

    Add files via upload
    Calculadora cod

---
+++ Calculator/src/br/com/dio/calculadora/Calculator.java
@@ -0,0 +1,45 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double a, b;
+
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+		double divisao = divisao(a, b);
+
+		System.out.println("O valor somado é: " + soma);
+		System.out.println("O valor subtraido é: " + subtracao);
+		System.out.println("O valor multiplicado é: " + multiplicacao);
+		System.out.println("O valor dividido é: " + divisao);
+
+	}
+
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+
+	public static double subtracao(double a, double b) {
+		return a - b;
+	}
+
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+	public static double divisao(double a, double b) {
+		return a / b;
+	}
+
+}
